ae9fd9985ba467a042e36a99614f2e85f34d9b79
Roger <sheqinglyh@163.com>
2017-09-19 15:45:09 +0800

    增加注释

--- src/roger/jdk/unittest/BigDecimalTest.java
+++ src/roger/jdk/unittest/BigDecimalTest.java
@@ -8,10 +8,24 @@ import java.math.BigDecimal;
 public class BigDecimalTest {
 
     @Test
+    /**
+     * 加法测试
+     */
     public void testAdd(){
         BigDecimal a = new BigDecimal("1");
         BigDecimal b = new BigDecimal("2");
         Assert.assertEquals(BigDecimal.valueOf(3),a.add(b));
+    }
+
+    @Test
+    /**
+     * 减法测试
+     */
+    public void testSubtract(){
+        BigDecimal a = new BigDecimal("1");
+        BigDecimal b = new BigDecimal("2");
+        Assert.assertEquals(BigDecimal.valueOf(1),b.subtract(a));
+
     }
 
 }
